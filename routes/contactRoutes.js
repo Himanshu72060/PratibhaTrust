f34ba8ad05ecda9048623c2a388af73aead9fb0c
Himanshu72060 <himanshusharma00898@gmail.com>
2025-06-11 17:07:06 +0530

    new code

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const Event = require('../models/Event');
-const upload = require('../middleware/upload');
+const Contact = require('../models/Contact');
 
-router.post('/', upload.single('image'), async (req, res) => {
+// POST
+router.post('/', async (req, res) => {
     try {
-        const event = new Event({ title: req.body.title, image: req.file.path });
-        await event.save();
-        res.status(201).json(event);
+        const contact = new Contact(req.body);
+        await contact.save();
+        res.status(201).json(contact);
     } catch (err) {
         res.status(400).json({ error: err.message });
     }
 });
 
+// GET
 router.get('/', async (req, res) => {
-    const events = await Event.find();
-    res.json(events);
+    const data = await Contact.find();
+    res.json(data);
 });
 
-router.put('/:id', upload.single('image'), async (req, res) => {
-    const updated = await Event.findByIdAndUpdate(req.params.id, {
-        title: req.body.title,
-        ...(req.file && { image: req.file.path })
-    }, { new: true });
-    res.json(updated);
+// PUT
+router.put('/:id', async (req, res) => {
+    const contact = await Contact.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(contact);
 });
 
+// DELETE
 router.delete('/:id', async (req, res) => {
-    await Event.findByIdAndDelete(req.params.id);
+    await Contact.findByIdAndDelete(req.params.id);
     res.json({ message: 'Deleted successfully' });
 });
 
